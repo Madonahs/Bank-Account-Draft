fe6ece6ce08bd46ae2e8acf4478ead11341ab7c0
Madona Syombua <syombuamadona@gmail.com>
2017-11-25 07:12:18 -0600

    Update UserTest.java

--- UserTest.java
+++ UserTest.java
@@ -4,9 +4,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
-
+import Root.User;
 public class UserTest {
-
+  User user;
 	@Before
 	public void setUp() throws Exception {
 	}
