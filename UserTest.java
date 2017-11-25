9427cb78c52bbf2d2f8918ad80ccff87b95bf683
Madona Syombua <syombuamadona@gmail.com>
2017-11-25 07:31:26 -0600

    Update UserTest.java

--- UserTest.java
+++ UserTest.java
@@ -13,57 +13,57 @@ public class UserTest {
 
 	@Test
 	public void testUser() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testAddAccount() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetUUID() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testValidatePin() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetFirstName() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testPrintAccountsSummary() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testNumAccounts() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testPrintAcctTransHistory() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetAcctBalance() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetAcctUUID() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testAddAcctTransaction() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 }
