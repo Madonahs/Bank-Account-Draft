95219dbfd19f917b197b0a92986cbb6548bc3579
Madona Syombua <syombuamadona@gmail.com>
2017-11-25 07:15:21 -0600

    Update TransactionTest.java

--- TransactionTest.java
+++ TransactionTest.java
@@ -4,9 +4,10 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
-
+import Root.Transaction;
 public class TransactionTest {
 
+	Transaction trans;
 	@Before
 	public void setUp() throws Exception {
 	}
