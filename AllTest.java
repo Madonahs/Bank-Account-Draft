3ac10440d9b2c310a410d6f2882f44eb9733abe4
Madona Syombua <syombuamadona@gmail.com>
2017-11-25 07:32:55 -0600

    Add files via upload

---
+++ AllTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package init;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author tmuthoka
+ *
+ */
+public class AllTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
