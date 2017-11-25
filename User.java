f1db11ff53a8c91c0885709d67b63b4323efadeb
Madona Syombua <syombuamadona@gmail.com>
2017-11-25 07:33:48 -0600

    Update User.java

--- User.java
+++ User.java
@@ -4,7 +4,13 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
+/**
 
+ * @author syombua
+
+ *
+
+ */
 public class User {
 
 	/**
