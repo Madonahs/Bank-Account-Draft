383a603192d528f390d7b429dd3692511c1d715a
Madona Syombua <syombuamadona@gmail.com>
2018-01-06 15:08:11 -0600

    Update UserTest.java

--- UserTest.java
+++ UserTest.java
@@ -20,50 +20,7 @@ public class UserTest {
 		
 	}*/
 
-	@Test
-	public void testAddAccount() {
-		//fail("Not yet implemented");
-	}
+	
 
-	@Test
-	public void testGetUUID() {
-		//fail("Not yet implemented");
-	}
-
-	@Test
-	public void testValidatePin() {
-		//fail("Not yet implemented");
-	}
-
-
-	@Test
-	public void testPrintAccountsSummary() {
-		//fail("Not yet implemented");
-	}
-
-	/*@Test
-	public void testNumAccounts() {
-		//fail("Not yet implemented");
-	}*/
-
-	@Test
-	public void testPrintAcctTransHistory() {
-		//fail("Not yet implemented");
-	}
-
-	/*@Test
-	public void testGetAcctBalance() {
-		//fail("Not yet implemented");
-	}*/
-
-	@Test
-	public void testGetAcctUUID() {
-		//fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAddAcctTransaction() {
-		//fail("Not yet implemented");
-	}
-
+	
 }
