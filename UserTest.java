cddb9d87f0c4d0b6b8cc7c7b33512b5b308833de
Madona Syombua <syombuamadona@gmail.com>
2018-01-17 20:02:15 -0600

    Update UserTest.java

--- UserTest.java
+++ UserTest.java
@@ -6,19 +6,14 @@ import org.junit.Before;
 import org.junit.Test;
 import Root.User;
 
-/*
-@author: syombua
-*/
+
 public class UserTest {
   User user;
 	@Before
 	public void setUp() throws Exception {
 	}
 
-	/*@Test
-	public void testUser() {
-		
-	}*/
+	
 
 	
 
